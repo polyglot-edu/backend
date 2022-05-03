7c5ab8992a3d47dd41b9a98e4d28e171d2421fe7
Tommaso Martorella <tom.martorella@gmail.com>
2022-05-03 12:23:41 +0200

    added initial exercise api

--- src/controllers/execution.controller.ts
+++ src/controllers/execution.controller.ts
@@ -1,25 +1,45 @@
 import { Request, Response } from "express";
 import { flows } from "../models/flow.model";
 
-type GetNextExerciseBody = {
-    id: string;
-    satisfiedConditions: string[];
-}
-export async function getNextExercise(req: Request<{}, any, GetNextExerciseBody>, res: Response) {
-    const { id, satisfiedConditions } = req.body;
-    const currentFlow = flows[id];
+type GetInitialExerciseBody = { flowId: string }
+export async function getInitialExercise(req: Request<{}, any, GetInitialExerciseBody>, res: Response) {
+    const { flowId } = req.body;
+    const currentFlow = flows[flowId];
 
     if (!currentFlow) {
         res.status(404).send();
         return;
     }
 
+    const nodesWithIncomingEdges = new Set(currentFlow.edges.map(edge => currentFlow.nodes.find(node => node.reactFlow.id === edge.reactFlow.target)));
+    const nodesWithoutIncomingEdges = currentFlow.nodes.filter(node => !nodesWithIncomingEdges.has(node));
+    const firstNode = nodesWithoutIncomingEdges[Math.floor(Math.random() * nodesWithoutIncomingEdges.length)];
+
+    if (!firstNode) {
+        res.status(404).send();
+        return;
+    }
+
+    res.status(200).json(firstNode);
+}
+
+type GetNextExerciseBody = {
+    flowId: string;
+    satisfiedConditions: string[];
+}
+export async function getNextExercise(req: Request<{}, any, GetNextExerciseBody>, res: Response) {
+    const { flowId, satisfiedConditions } = req.body;
+    const currentFlow = flows[flowId];
+
+    if (!currentFlow) {
+        res.status(404).send();
+        return;
+    }
 
     const satisfiedEdges = currentFlow.edges.filter(edge => satisfiedConditions.includes(edge.reactFlow.id));
-    const possibleNextNodes = satisfiedEdges.map(edge => currentFlow.nodes[edge.reactFlow.target]);
+    const possibleNextNodes = satisfiedEdges.map(edge => currentFlow.nodes.find(node => node.reactFlow.id === edge.reactFlow.target));
     const nextNode = possibleNextNodes[Math.floor(Math.random() * possibleNextNodes.length)];
 
-    // console.log(JSON.stringify(satisfiedEdges, null, 2))
     if (!nextNode) {
         res.status(404).send();
         return;
--- src/routes/execution.routes.ts
+++ src/routes/execution.routes.ts
@@ -4,5 +4,6 @@ import * as ExecutionController from "../controllers/execution.controller";
 const router = express.Router();
 
 router.get("/next", ExecutionController.getNextExercise)
+router.get("/first", ExecutionController.getInitialExercise)
 
 export default router;
