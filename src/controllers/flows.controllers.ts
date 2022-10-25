a2e6343adaea85bf02ca9178ec5d66a0cae2fcc3
daviFrag <davide.frag@gmail.com>
2022-10-25 19:27:45 +0200

    implement mongoose database + rewrite controllers

--- src/controllers/flows.controllers.ts
+++ src/controllers/flows.controllers.ts
@@ -1,20 +1,25 @@
-import { Request, Response } from "express";
-import { flows } from "../models/flow.model";
-import { PolyglotFlow } from "../types";
+import { NextFunction, Request, Response } from "express";
+import PolyglotFlowModel from "../models/flow.model";
+import { PolyglotFlow } from "PolyglotFlow";
+import { Document } from "mongoose";
 
 /*
     Get flow by id
     @route GET /flows/:id
 */
-export async function getFlowById(req: Request<{ id: string }>, res: Response<PolyglotFlow>) {
+export async function getFlowById(req: Request<{ id: string }>, res: Response<Document<unknown, any, PolyglotFlow> & PolyglotFlow>, next : NextFunction) {
     // await param("id", "UUID v4 is required").isUUID(4).run(req);
 
-    const flow = flows[req.params.id];
-    if (!flow) {
-        res.status(404).send();
-        return;
+    try {
+      const flow = await PolyglotFlowModel.findById(req.params.id)
+      if (!flow) {
+        return res.status(404).send();
+      }
+      return res.status(200).send(flow);
+    } catch (err) {
+      return next(err);
     }
-    res.send(flow);
+    
 }
 
 
@@ -22,31 +27,33 @@ export async function getFlowById(req: Request<{ id: string }>, res: Response<Po
     Update flow with given id
     @route PUT /flows/:id
 */
-export async function updateFlow(req: Request<{ id: string }>, res: Response) {
+export async function updateFlow(req: Request<{ id: string }>, res: Response, next: NextFunction) {
     // await param("id", "UUID v4 is required").isUUID(4).run(req);
     // TODO: FIXME: custom validation here for flow
     // await body("flow", "Flow is required").exists().run(req);
 
-    const flow = flows[req.params.id];
-    if (!flow) {
-        res.status(404).send();
-        return;
+    try {
+      const flow = await PolyglotFlowModel.updateOne({
+        _id: req.params.id
+      },req.body)
+      if (!flow) {
+        return res.status(404).send()
+      }
+      return res.status(200).send();
+    } catch(err) {
+      return next(err);
     }
-    flows[req.params.id] = req.body;
-    res.status(200).send();
+    
 }
 
-export async function createFlow(req: Request<PolyglotFlow>, res: Response) {
+export async function createFlow(req: Request<PolyglotFlow>, res: Response, next : NextFunction) {
     // TODO: add validation for flow
     const newFlow = req.body;
 
-    if (flows[newFlow.id]) {
-        res.status(409).send();
-        return;
+    try {
+      const flow =  await PolyglotFlowModel.create(newFlow);
+      return res.status(200).send({id: flow._id});
+    } catch (err) {
+      return next(err);
     }
-
-    flows[newFlow.id] = newFlow;
-    res.status(200).send({
-        id: newFlow.id,
-    });
 }
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import errorHandler from "errorhandler";
 import app from "./app";
+import mongoose from 'mongoose';
 
 /**
  * Error Handler. Provides full stack
@@ -9,10 +10,17 @@ if (app.get("env") === "development") {
     app.use(errorHandler());
 }
 
+const MONGO_URL = process.env.MONGO_URL || 'mongodb://prova:SECRET@mongodb:27017';
+
 /**
  * Start Express server.
  */
-const server = app.listen(app.get("port"), () => {
+const server = app.listen(app.get("port"), async () => {
+    await mongoose.connect(MONGO_URL)
+    .then(() => console.log("  Database connected!\n"))
+    .catch((error) => {
+        console.log(error);
+    });
     console.log(
         "  App is running at http://localhost:%d in %s mode  🚀🚀",
         app.get("port"),
