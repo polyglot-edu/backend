0025719dd3df580ac8a22b7ad154854c15b06b96
daviFrag <davide.frag@gmail.com>
2022-11-25 02:31:31 +0100

    fix: file permissions

--- src/utils/secrets.ts
+++ src/utils/secrets.ts
@@ -8,12 +8,12 @@ export const ENV = process.env.NODE_ENV || "development";
 
 export const PORT = (process.env.PORT || 5000) as number;
 
-export const MONGO_URL = (process.env.MONGODB_URI || 'mongodb://prova:SECRET@localhost:27017') + (process.env.MONGODB_CERTIFICATE ? encodeURIComponent(`${__dirname}/certificate.pem`) : "");
+export const MONGO_URL = (process.env.MONGODB_URI || 'mongodb://prova:SECRET@localhost:27017') + (process.env.MONGODB_CERTIFICATE ? encodeURIComponent(`/tmp/certificate.pem`) : "");
 
 export const MONGODB_CERTIFICATE = (process.env.MONGODB_CERTIFICATE) as string;
 
 if (MONGODB_CERTIFICATE) {
-    fs.writeFileSync(`${__dirname}/certificate.pem`, MONGODB_CERTIFICATE)
+    fs.writeFileSync(`/tmp/certificate.pem`, MONGODB_CERTIFICATE)
 }
 
 export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID as string;
