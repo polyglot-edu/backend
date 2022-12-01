8819e25bfac38af8031b0544468c7f9667230236
daviFrag <davide.frag@gmail.com>
2022-12-01 22:32:32 +0100

    refactor: better handling secrets + remove cached connection

--- src/utils/secrets.ts
+++ src/utils/secrets.ts
@@ -4,17 +4,16 @@ import fs from 'fs';
 
 dotenv.config();
 
+// Env definition
 export const ENV = process.env.NODE_ENV || "development";
 
-export const PORT = (process.env.PORT || 5000) as number;
+export const PORT = process.env.PORT ? +process.env.PORT : 5000;
 
-export const MONGO_URL = (process.env.MONGODB_URI || 'mongodb://prova:SECRET@localhost:27017') + (process.env.MONGODB_CERTIFICATE ? encodeURIComponent(`/tmp/certificate.pem`) : "");
+const MONGODB_URI = process.env.MONGODB_URI;
 
-export const MONGODB_CERTIFICATE = (process.env.MONGODB_CERTIFICATE) as string;
+const MONGODB_CERTIFICATE = process.env.MONGODB_CERTIFICATE;
 
-if (MONGODB_CERTIFICATE) {
-    fs.writeFileSync(`/tmp/certificate.pem`, MONGODB_CERTIFICATE)
-}
+export const MONGO_URL = MONGODB_URI + (MONGODB_CERTIFICATE ? encodeURIComponent(`/tmp/certificate.pem`) : "");
 
 export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID as string;
 
@@ -22,4 +21,21 @@ export const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET as string;
 
 export const COOKIE_KEY = process.env.COOKIE_KEY as string;
 
-export const CORS_ORIGINS = process.env.CORS_ORIGINS?.split(',') as string[]
+export const CORS_ORIGINS = process.env.CORS_ORIGINS?.split(',') as string[];
+
+
+// Env check
+if (!CORS_ORIGINS) throw new Error("CORS_ORIGINS env not defined!");
+
+if (!MONGODB_URI) throw new Error("MONGODB_URI env not defined!");
+
+if (!GOOGLE_CLIENT_ID) throw new Error("GOOGLE_CLIENT_ID env not defined!");
+
+if (!GOOGLE_CLIENT_SECRET) throw new Error("GOOGLE_CLIENT_SECRET env not defined!");
+
+if (!COOKIE_KEY) throw new Error("COOKIE_KEY env not defined!");
+
+// Env rielaboration
+if (MONGODB_CERTIFICATE) {
+    fs.writeFileSync(`/tmp/certificate.pem`, MONGODB_CERTIFICATE)
+}
