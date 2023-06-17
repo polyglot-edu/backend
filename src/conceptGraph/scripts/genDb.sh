e42b7ab8ce2efe0f01ae2f8c8c68a409f2fc16af
Davide Frageri <davide.frag@gmail.com>
2023-06-17 23:54:40 +0200

    fix: bug docker build fail

--- src/conceptGraph/scripts/genDb.sh
+++ src/conceptGraph/scripts/genDb.sh
@@ -1,7 +1,7 @@
 npx ts-node ./src/conceptGraph/migrations.ts && \
-npx ts-node ./src/conceptGraph/scripts/cleanCsv.ts && \
-sqlite3 encoreDB ".import --csv --skip 1 ./src/conceptGraph/material/concepts_lexicon.csv concepts_lexicon" && \
-sqlite3 encoreDB ".import --csv --skip 1 ./src/conceptGraph/tmp/OER_and_wiki_relationships_hyperlinks_NEW.csv oers_wiki" && \
-sqlite3 encoreDB ".import --csv --skip 1 ./src/conceptGraph/tmp/encore_oers.csv encore_oers" && \
-sqlite3 encoreDB ".import --csv --skip 1 ./src/conceptGraph/tmp/OER_and_concept_table.csv oers_concept" && \
+npx ts-node ./src/conceptGraph/scripts/cleanCsv.ts ; \
+sqlite3 encoreDB ".import --csv --skip 1 ./src/conceptGraph/material/concepts_lexicon.csv concepts_lexicon" ; \
+sqlite3 encoreDB ".import --csv --skip 1 ./src/conceptGraph/tmp/OER_and_wiki_relationships_hyperlinks_NEW.csv oers_wiki" ; \
+sqlite3 encoreDB ".import --csv --skip 1 ./src/conceptGraph/tmp/encore_oers.csv encore_oers" ; \
+sqlite3 encoreDB ".import --csv --skip 1 ./src/conceptGraph/tmp/OER_and_concept_table.csv oers_concept" ; \
 rm -r ./src/conceptGraph/tmp
