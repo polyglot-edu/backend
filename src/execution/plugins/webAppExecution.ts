f2499b61955aaa32c3612845ea4e65c60098c688
tmaog <tommasoguidolin01@gmail.com>
2023-12-14 10:35:35 +0100

    webApp execution implementation

--- src/execution/plugins/webAppExecution.ts
+++ src/execution/plugins/webAppExecution.ts
@@ -4,101 +4,93 @@ import { ChallengeContent, ChallengeSetup, LessonTextNodeData, MultipleChoiceQue
 //LessonTextNodeData Execution block
 
 function lessonTextNodeExecution(node:PolyglotNode){
-const oldData = node.data as LessonTextNodeData;
-const webAppSetup: WebAppSetup[] = [];
-const webAppContent: WebAppContent[] = [
-    {
-    content: oldData.text,
-    },
-];
-
-return {
-    ...node,
-    runtimeData: {
-    webAppSetup,
-    webAppContent,
-    },
-};}
+  const oldData = node.data as LessonTextNodeData;
+  const webAppSetup: WebAppSetup[] = [];
+  const webAppContent: WebAppContent[] = [
+      {
+      content: oldData.text,
+      },
+  ];
+  return {
+    webAppSetup, 
+    webAppContent,        
+};
+}
 
 //readMaterialNode Execution block
 
 function readMaterialNodeExecution(node:PolyglotNode){
-    const oldData = node.data as readMaterialNodeData;
+  const oldData = node.data as readMaterialNodeData;
 
-    const webAppSetup: WebAppSetup[] = [];
-    const webAppContent: WebAppContent[] = [
-      {
-        content: oldData.text,
-      },
-      {
-        content: 'Run this link: ' + oldData.link,
-      },
-    ];
+  const webAppSetup: WebAppSetup[] = [];
+  const webAppContent: WebAppContent[] = [
+    {
+      content: oldData.text,
+    },
+    {
+      content: 'Run this link: ' + oldData.link,
+    },
+  ];
 
-    return {
-      ...node,
-      runtimeData: {
-        webAppSetup,
-        webAppContent,
-      },
-    };}
+  return {
+    webAppSetup, 
+    webAppContent,        
+  };
+}
 
 //closeEndedQuestionNode Execution block 
-    function closeEndedQuestionNodeExecution(node:PolyglotNode){
-    const oldData = node.data as closeEndedQuestionNodeData;
+function closeEndedQuestionNodeExecution(node:PolyglotNode){
+  const oldData = node.data as closeEndedQuestionNodeData;
 
-    const webAppSetup: WebAppSetup[] = [];
-    const webAppContent: WebAppContent[] = [
-        {
-        content: oldData?.question,
-        priority: 0,
-        },
-        {
-        type: 'html',
-        content: '',
-        priority: 1,
-        },
-    ];
+  const webAppSetup: WebAppSetup[] = [];
+  const webAppContent: WebAppContent[] = [
+      {
+      content: oldData?.question,
+      priority: 0,
+      },
+      {
+      type: 'html',
+      content: '',
+      priority: 1,
+      },
+  ];
 
-    return {
-        ...node,
-        runtimeData: {
-        webAppSetup,
-        webAppContent,
-        },
-    };
+  return {
+    webAppSetup, 
+    webAppContent,        
+  };
 }
 
 //MultipleChoiceQuestionNodeData Execution block  
 function multipleChoiceQuestionNodeExecution(node:PolyglotNode){
-    const specificData = node.data as MultipleChoiceQuestionNodeData;
-    const data = {
-        ...specificData,
-        correctAnswers: zip(specificData?.choices, specificData?.isChoiceCorrect).reduce(
-        (acc, { first, second }) => {
-            if (second) {
-            acc.push(first);
-            }
-            return acc;
-        },
-        [] as string[]
-        ),
-    };
-    const webAppSetup: WebAppSetup[] = [];
-    const webAppContent: WebAppContent[] = [];    
+  const data = node.data as MultipleChoiceQuestionNodeData;
+  const webAppSetup: WebAppSetup[] = [];
+  const webAppContent: WebAppContent = {content: data, type: 'MultChoiceQuestion'};    
 
-    return {
-        ...node,
-        data,
-        runtimeData: {specificData
-        },
-};}
+  return {
+    webAppSetup, 
+    webAppContent,        
+  };
+}
 
 export function webAppExecution(node:PolyglotNode){
-    console.log('webApp execution run');
-    if(node?.type=="multipleChoiceQuestionNode") return multipleChoiceQuestionNodeExecution(node);
-    if(node?.type=="lessonTextNode") {console.log('lessonText'); return lessonTextNodeExecution(node);}
-    if(node?.type=="closeEndedQuestionNode") return closeEndedQuestionNodeExecution(node);
-    if(node?.type=="ReadMaterialNode") return readMaterialNodeExecution(node);
-    return null;
+  const challengeSetup: ChallengeSetup[] = [];
+  const challengeContent : ChallengeContent [] = [
+      {
+      type: 'markdown',
+      content: 'This node need to be executed in WebApp: https://polyglot-api.polyglot-edu.com/api/execution/next/'+node._id,
+      },
+  ];
+  let webAppSpecifics;
+  if(node?.type=="multipleChoiceQuestionNode")  webAppSpecifics=multipleChoiceQuestionNodeExecution(node);
+  if(node?.type=="lessonTextNode") webAppSpecifics=lessonTextNodeExecution(node);
+  if(node?.type=="closeEndedQuestionNode") webAppSpecifics=closeEndedQuestionNodeExecution(node);
+  if(node?.type=="ReadMaterialNode") webAppSpecifics=readMaterialNodeExecution(node);
+  return {...node,
+  runtimeData: {
+    challengeSetup,
+    challengeContent,
+    webAppSpecifics
+  },
+  }
 }
